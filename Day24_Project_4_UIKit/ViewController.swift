8aef18ea00254684ef53be2c326a483f3db3791c
Sandy Gomez <sgomez1029@gmail.com>
2024-02-29 22:19:51 -0800

    Setting Up, Creating a simple browser with WKWebView, Chooseing a website: UIAlertControllerActionSheets

--- Day24_Project_4_UIKit/ViewController.swift
+++ Day24_Project_4_UIKit/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let url = URL(string: "https://www.radiohead.com")!
+        webView.load(URLRequest(url:url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
     }
-
-
+    
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "radiohead.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "pg-lang.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https//" + action.title!)!
+        webView.load(URLRequest(url:url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 }
 
