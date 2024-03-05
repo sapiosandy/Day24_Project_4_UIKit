91962e8ddfa1704fdf5c4612d51f92bc672540fa
Sandy Gomez <sgomez1029@gmail.com>
2024-03-04 18:32:01 -0800

    Edited Code - works now!

--- Day24_Project_4_UIKit/ViewController.swift
+++ Day24_Project_4_UIKit/ViewController.swift
@@ -49,7 +49,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
     }
     
     func openPage(action: UIAlertAction) {
-        let url = URL(string: "https//" + action.title!)!
+        let url = URL(string: "https://" + action.title!)!
         webView.load(URLRequest(url:url))
     }
     
